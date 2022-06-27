ef1832f5edacc26c4c37f3ac694fec80bcd513ed
Alex Bennett <alexandergraybennett@gmail.com>
2022-06-27 15:46:21 -0600

    granular caches for deps and build
    * granular caches for deps and build

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Cookies from "js-cookie";
+
+Cookies.set("foo", "bar");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Source code change!</h1>
       </header>
     </div>
   );
